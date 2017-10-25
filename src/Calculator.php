85578f91f6e75ee5914bf2944b1a3b98b113c301
deuterium7 <post0778@gmail.com>
2017-10-25 11:55:24 +0300

    Добавил отступы в аннотации

--- src/Calculator.php
+++ src/Calculator.php
@@ -9,6 +9,7 @@ class Calculator
      *
      * @param float $a
      * @param float $b
+     *
      * @return float
      */
     public function add(float $a, float $b)
@@ -21,6 +22,7 @@ class Calculator
      *
      * @param float $a
      * @param float $b
+     *
      * @return float
      */
     public function subtract(float $a, float $b)
@@ -33,6 +35,7 @@ class Calculator
      *
      * @param float $a
      * @param float $b
+     *
      * @return float
      */
     public function multiply(float $a, float $b)
@@ -45,6 +48,7 @@ class Calculator
      *
      * @param float $a
      * @param float $b
+     *
      * @return float
      */
     public function divide(float $a, float $b)
